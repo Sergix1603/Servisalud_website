8375c8ff97d29c18f607c810f835206b5b2a975f
Adrian52507 <adrian52507@gmail.com>
2023-09-24 14:58:42 -0500

    Vistas

--- ServiSalud1/Controllers/EmpleadosController.cs
+++ ServiSalud1/Controllers/EmpleadosController.cs
@@ -1,12 +1,43 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ServiSalud1.Models;
+using ServiSalud1.Datos;
 
 namespace ServiSalud1.Controllers
 {
     public class EmpleadosController : Controller
     {
+        public readonly ApplicationDbContext objCat;
+        public EmpleadosController(ApplicationDbContext dbContext)
+        {
+            objCat = dbContext;
+        }
         public IActionResult Index()
         {
+
+            List<Empleados> listaEmpleados = objCat.Empleados.ToList();
+            return View(listaEmpleados);
+        }
+
+        [HttpGet]
+        public IActionResult Crear()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public IActionResult Crear(Empleados Empleados)
+        {
+            if (ModelState.IsValid)
+            {
+                objCat.Empleados.Add(Empleados);
+                objCat.SaveChanges();
+                return RedirectToAction("Index");
+            }
             return View();
         }
     }
 }
+
