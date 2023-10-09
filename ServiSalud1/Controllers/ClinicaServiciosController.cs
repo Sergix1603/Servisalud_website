3948e906d19b9772fa40aa3e890463888a92d52e
Adrian52507 <adrian52507@gmail.com>
2023-10-09 12:11:13 -0500

    Primer ViewModel

---
+++ ServiSalud1/Controllers/ClinicaServiciosController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ServiSalud1.Datos;
+using ServiSalud1.ViewModels;
+
+namespace ServiSalud1.Controllers
+{
+    public class ClinicaServiciosController : Controller
+    {
+        public readonly ApplicationDbContext objCliServ;
+        public ClinicaServiciosController(ApplicationDbContext dbContext)
+        {
+            objCliServ = dbContext;
+        }
+        public IActionResult Index()
+        {
+            var viewclinicaservicios = (from cs in objCliServ.Clinica_servicios
+                                        join c in objCliServ.Clinica on cs.Id_clinica equals c.Id_Clinica
+                                        join s in objCliServ.Servicios on cs.Id_serv equals s.Id_serv
+                                        select new ClinicaServiciosViewModel{
+                                            Nombre_Clinica = c.Nombre_clinica,
+                                            Ubicacion = c.Ubicacion,
+                                            Nombre_Servicio = s.Nom_serv
+                                         }).ToList();
+            return View(viewclinicaservicios);
+        }
+
+    }
+}
---
+++ ServiSalud1/ViewModels/ClinicaServiciosViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace ServiSalud1.ViewModels
+{
+    public class ClinicaServiciosViewModel
+    {
+        [MaxLength(30)]
+        public string Nombre_Clinica { get; set; }
+        [MaxLength(30)]
+        public string Ubicacion { get; set; }
+        [MaxLength(50)]
+        public string Nombre_Servicio { get; set; }
+    }
+}
