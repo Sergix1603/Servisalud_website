f063c8d0460cd38fbce5110579cbf63b6a5eade3
Sergio Burga <sergiobuce04@gmail.com>
2023-10-24 14:30:09 -0500

    Login empleado

--- ServiSalud1/Controllers/HomeController.cs
+++ ServiSalud1/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace ServiSalud1.Controllers
 
         public IActionResult Index()
         {
-            return View("Login_paciente");//Aqui se configura pa cambiar vistas, dentro de los parentesis
+            return View("Login_empleado");//Aqui se configura pa cambiar vistas, dentro de los parentesis
 
         }
         public IActionResult Contacto()
